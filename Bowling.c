cd1ff00ce3e13130622c0c54f2882eb2b7431a4a
TheVirtualMachine <wongtech@vincemacri.ca>
2017-09-20 11:28:53 -0400

    Some stuff, I forget.

--- Bowling.c
+++ Bowling.c
@@ -14,7 +14,7 @@
 #define RIGHT (!LEFT)
 
 // Define the colour ranges.
-#define BLACK_END 37
+#define BLACK_END 40
 #define GREEN_START BLACK_END
 #define GREEN_END 50
 #define WHITE_START GREEN_END
@@ -23,13 +23,11 @@
 #define HIGH 30
 #define LOW 2
 
-// The current direction we are turning.
-bool currentDir;
-// The direction we were going when last on black.
-bool lastDir;
-
 // The value of the light sensor on this tick.
 int light;
+
+// The number of green stops that have been passed.
+int stopsPassed = 0;
 
 // Return whether or not we are currently on black.
 bool onBlack() {
@@ -47,13 +45,11 @@ bool onWhite() {
 }
 
 void goLeft() {
-	currentDir = LEFT;
 	motor[motorB] = HIGH; // Set right motor to low.
 	motor[motorC] = LOW; // Set left motor to high.
 }
 
 void goRight() {
-	currentDir = RIGHT;
 	motor[motorB] = LOW; // Set right motor to low.
 	motor[motorC] = HIGH; // Set left motor to high.
 }
@@ -67,14 +63,20 @@ void setDirection(bool newDir) {
 	}
 }
 
+// Stop moving.
+void stop() {
+	motor[motorB] = 0;
+	motor[motorC] = 0;
+}
+
+// Shoot the projectile.
 void shoot() {
 	motor[motorA] = 100;
 }
 
 task main() {
 	wait1Msec(50); // The program waits 50 milliseconds to initialize the light sensor.
-	currentDir = RIGHT;
-	setDirection(currentDir);
+	setDirection(RIGHT);
 	while (true) {
 		light = SensorValue[lightSensor];
 		if (onBlack()) {
@@ -82,6 +84,7 @@ task main() {
 		} else if (onWhite()) {
 			setDirection(LEFT);
 		} else {
+			stop();
 			break;
 		}
 	}
